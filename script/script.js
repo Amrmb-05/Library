4652b9ad0ae7e6320a1c9a883d23ceef3a05e29b
Amr Mohamed <amoree.mb@gmail.com>
2022-03-01 22:29:33 +0200

    wrote a function that displays each book in the page in cards and styed the cards

--- script/script.js
+++ script/script.js
@@ -1,4 +1,7 @@
-let myLibrary = [];
+const onePiece = new Book('One Piece', 'Eichiro Oda', "1041", 'Not read')
+let myLibrary = [onePiece];
+
+
 
 function Book(title, author, pages, read) {
   this.title = title
@@ -7,6 +10,39 @@ function Book(title, author, pages, read) {
   this.read = read
 }
 
-function addBookToLibrary() {
-  
+function addBookToLibrary(ev) {
+  ev.preventDefault();
+  let newBook = new Book(
+    document.getElementById('title').value,
+    document.getElementById('author').value,
+    document.getElementById('pages').value,
+    document.getElementById('read').value
+  ) 
+  myLibrary.push(newBook);
+  document.querySelector('form').reset();
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('submit-btn').addEventListener('click', addBookToLibrary)
+})
+
+const container = document.getElementById('card-holder');
+function addBookCard(ev) {
+    myLibrary.forEach(book => {
+    console.log(book)
+    const card = document.createElement('div');
+    card.classList = 'card-body';
+    const content = `
+    <div class="card-body">
+          <h5>${book.title}</h5>
+          <p>${book.author}</p>
+          <p>${book.pages}</p>
+          <p>${book.read}</p>
+        </div>`;
+    container.innerHTML += content;
+    myLibrary = []
+})}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('submit-btn').addEventListener('click', addBookCard)
+})
