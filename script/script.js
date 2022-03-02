0bd9e288acae77f7aee4c8109d0eda1b20d318a1
Amr Mohamed <amoree.mb@gmail.com>
2022-03-02 19:33:17 +0200

    add a remove button

--- script/script.js
+++ script/script.js
@@ -1,5 +1,4 @@
-const onePiece = new Book('One Piece', 'Eichiro Oda', "1041", 'Not read')
-let myLibrary = [onePiece];
+let myLibrary = [];
 
 
 
@@ -27,22 +26,45 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 const container = document.getElementById('card-holder');
+
 function addBookCard(ev) {
+    
+    
     myLibrary.forEach(book => {
+    
     console.log(book)
-    const card = document.createElement('div');
+    let card = document.createElement('div');
     card.classList = 'card-body';
+      
     const content = `
-    <div class="card-body">
-          <h5>${book.title}</h5>
+    <div class="card-body" data-index="${myLibrary.indexOf(book)}">
+          <h4>${book.title}</h5>
           <p>${book.author}</p>
           <p>${book.pages}</p>
           <p>${book.read}</p>
+          <button class='remove-book-btn' data-index="${myLibrary.indexOf(book)}">Remove</button>
         </div>`;
+    
     container.innerHTML += content;
     myLibrary = []
+    document.querySelectorAll('.remove-book-btn').forEach(btn => {btn.addEventListener('click',() => {
+    card = btn.closest("div")
+    const parent = card.parentNode;
+    parent.removeChild(card);})})
+    
 })}
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('submit-btn').addEventListener('click', addBookCard)
 })
+
+function openForm() {
+  document.getElementById('form-container').style.display = 'flex';
+}
+
+function closeForm() {
+  document.getElementById('form-container').style.display = 'none';
+}
+
